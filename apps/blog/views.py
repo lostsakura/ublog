1066564fc68ab3fde2c216ae49a6711ecc6e1669
lostsakura <ldx9709@gmail.com>
2020-02-09 21:51:26 +0800

    修改了模板的结构 计划使用iframe实现不刷新页面的选项卡切换效果 所有选项卡的路由和模板已经设计好

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -3,9 +3,56 @@ from django.shortcuts import render
 # Create your views here.
 
 
+# 首页
 def blog_index(request):
     return render(request, 'index.html')
 
 
+# 后台
 def blog_admin(request):
-    return render(request, 'admin.html')
+    return render(request, 'admin_main.html')
+
+
+# 概要
+def admin_index(request):
+    return render(request, 'admin_index.html')
+
+
+# 文章编辑
+def write_article(request):
+    return render(request, 'admin_write_article.html')
+
+
+# 独立页面编辑
+def write_page(request):
+    return render(request, 'admin_write_page.html')
+
+
+# 文章管理
+def manage_articles(request):
+    return render(request, 'admin_manage_articles.html')
+
+
+# 独立页面管理
+def manage_pages(request):
+    return render(request, 'admin_manage_pages.html')
+
+
+# 评论管理
+def manage_comments(request):
+    return render(request, 'admin_manage_comments.html')
+
+
+# 标签管理
+def manage_labels(request):
+    return render(request, 'admin_manage_labels.html')
+
+
+# 个人设置
+def user_setup(request):
+    return render(request, 'admin_user_setup.html')
+
+
+# 系统设置
+def system_setup(request):
+    return render(request, 'admin_system_setup.html')
--- ublog/urls.py
+++ ublog/urls.py
@@ -21,8 +21,31 @@ urlpatterns = [
 
     # index页面
     path('', v.blog_index),
-    # admin页面
+    # 后台管理
     path('admin/', v.blog_admin),
+
+    # 概要
+    path('admin/admin-index/', v.admin_index),
+
+    # 文章编辑
+    path('admin/write-article', v.write_article),
+    # 独立页面编辑
+    path('admin/write-page', v.write_page),
+
+    # 文章管理
+    path('admin/manage-articles', v.manage_articles),
+    # 独立页面管理
+    path('admin/manage-pages', v.manage_pages),
+    # 评论管理
+    path('admin/manage-comments', v.manage_comments),
+    # 标签管理
+    path('admin/manage-labels', v.manage_labels),
+
+    # 个人设置
+    path('admin/user-setup', v.user_setup),
+    # 系统设置
+    path('admin/system-setup', v.system_setup),
+
 
 
 ]
